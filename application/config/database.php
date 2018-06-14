36cc261463ed1c5c03aeef2849e81faa0670ca04
Nobru44 <39130278+Nobru44@users.noreply.github.com>
2018-06-14 10:19:12 +0200

    first commit

---
+++ application/config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Database configuration settings used by PDO.
+ */
+
+$config['dsn']      = 'mysql:host=localhost;dbname=restaurant';
+$config['password'] = 'cuba2018';
+$config['user']     = 'nobru974';
